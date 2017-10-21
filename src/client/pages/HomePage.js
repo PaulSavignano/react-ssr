791094b63e694e8e603b0189a22e2d7c7ee5357d
Paul Savignano <Paul.Savignano@gmail.com>
2017-10-21 12:35:08 -0700

    Rehydrate client state

--- src/client/pages/HomePage.js
+++ src/client/pages/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
-const Home = () => (
+const HomePage = () => (
   <div>
     <div>I'm the BEST home component</div>
     <button onClick={() => console.log('Hi there!')}>Press me!</button>
   </div>
 )
 
-export default Home
+export default {
+  component: HomePage
+}
--- src/client/pages/UsersListPage.js
+++ src/client/pages/UsersListPage.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { fetchUsers } from '../actions'
 
-class UsersList extends Component {
+class UsersListPage extends Component {
   componentDidMount() {
     this.props.fetchUsers()
   }
@@ -29,6 +29,8 @@ const mapStateToProps = ({ users }) => ({
 function loadData(store) {
   return store.dispatch(fetchUsers())
 }
-export { loadData }
 
-export default connect(mapStateToProps, { fetchUsers })(UsersList)
+export default {
+  loadData,
+  component: connect(mapStateToProps, { fetchUsers })(UsersListPage)
+}
